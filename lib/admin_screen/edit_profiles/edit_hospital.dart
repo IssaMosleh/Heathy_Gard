b39229a9fc02b708cbea0a031ed09730c8d0c0c1
IssaMosleh <issamosleh2002@gmail.com>
2024-11-13 01:20:59 +0300

    comment

---
+++ lib/admin_screen/edit_profiles/edit_hospital.dart
@@ -0,0 +1,292 @@
+import 'package:flutter/material.dart';
+import 'package:tess/admin_screen/edit_profiles/edit_profiles_main.dart';
+import 'package:tess/insurance_screen/Hospitals/hospital_info.dart';
+
+class EditHospital extends StatelessWidget {
+  const EditHospital({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SearchScreen(),
+    );
+  }
+}
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  // Hospital data with an "inNetwork" field and city field
+  final List<Map<String, dynamic>> hospitals = [
+    {"name": "Jordan Hospital", "location": "Amman", "image": "images/hospital_image.png", "inNetwork": true, "city": "Amman"},
+    {"name": "Specialty Hospital", "location": "Amman", "image": "images/hospital_image.png", "inNetwork": false, "city": "Amman"},
+    {"name": "Al Khalidi Hospital", "location": "Amman", "image": "images/hospital_image.png", "inNetwork": true, "city": "Amman"},
+    {"name": "Zarqa Governmental Hospital", "location": "Zarqa", "image": "images/hospital_image.png", "inNetwork": false, "city": "Zarqa"},
+    {"name": "King Abdullah University Hospital", "location": "Irbid", "image": "images/hospital_image.png", "inNetwork": true, "city": "Irbid"},
+    {"name": "Ajloun Governmental Hospital", "location": "Ajloun", "image": "images/hospital_image.png", "inNetwork": true, "city": "Ajloun"},
+    {"name": "Al Karak Hospital", "location": "Karak", "image": "images/hospital_image.png", "inNetwork": false, "city": "Karak"},
+    {"name": "Aqaba Hospital", "location": "Aqaba", "image": "images/hospital_image.png", "inNetwork": true, "city": "Aqaba"},
+    {"name": "Madaba Governmental Hospital", "location": "Madaba", "image": "images/hospital_image.png", "inNetwork": true, "city": "Madaba"},
+    {"name": "Jerash Hospital", "location": "Jerash", "image": "images/hospital_image.png", "inNetwork": false, "city": "Jerash"},
+    {"name": "Salt Hospital", "location": "Salt", "image": "images/hospital_image.png", "inNetwork": true, "city": "Salt"},
+    {"name": "Mafraq Hospital", "location": "Mafraq", "image": "images/hospital_image.png", "inNetwork": true, "city": "Mafraq"},
+  ];
+
+  final List<String> cities = ["All Cities", "Amman", "Zarqa", "Irbid", "Ajloun", "Karak", "Aqaba", "Madaba", "Jerash", "Salt", "Mafraq", "Tafileh", "Balqa", "Maan"];
+  String selectedCity = "All Cities";
+  final List<String> recentSearches = [];
+  List<Map<String, dynamic>> searchResults = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+    _fetchAllHospitals();
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  // Fetch all hospitals based on selected city
+  void _fetchAllHospitals() {
+    setState(() {
+      searchResults = hospitals.where((hospital) {
+        return selectedCity == "All Cities" || hospital['city'] == selectedCity;
+      }).toList();
+    });
+  }
+
+  void _onSearchChanged() {
+    final query = _searchController.text;
+    if (query.isNotEmpty) {
+      setState(() {
+        searchResults = hospitals
+            .where((hospital) =>
+                hospital["name"]!.toLowerCase().contains(query.toLowerCase()) &&
+                (selectedCity == "All Cities" || hospital['city'] == selectedCity))
+            .toList();
+      });
+    } else {
+      _fetchAllHospitals();
+    }
+  }
+
+  void _onSearchSubmitted(String query) {
+    if (query.isNotEmpty) {
+      setState(() {
+        if (recentSearches.contains(query)) {
+          recentSearches.remove(query);
+        }
+        recentSearches.insert(0, query);
+        if (recentSearches.length > 3) {
+          recentSearches.removeLast();
+        }
+      });
+      _onSearchChanged();
+    }
+  }
+
+  void _onRecentSearchTapped(String query) {
+    _searchController.text = query;
+    _onSearchSubmitted(query);
+  }
+
+  void _onCardTap(Map<String, dynamic> hospital) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => HospitalStatsScreen(
+          hospitalName: "Jordan Hospital",
+          totalClaims: 120,
+          approvedClaims: 90,
+          rejectedClaims: 30,
+          totalBookings: 200,
+          totalDoctors: 50,
+          totalMoneySpent: 150000.0,
+        ),
+      ),
+    );
+    print("Tapped on ${hospital['name']}");
+  }
+
+  // Handle city filter selection
+  void _onCitySelected(String? city) {
+    setState(() {
+      selectedCity = city ?? "All Cities";
+      _fetchAllHospitals();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Choose Hospital",
+          style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        leading: IconButton(
+          onPressed: () {
+             Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const edit_profiles_main()),
+                  );
+          },
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.purple], // Gradient color from My_Info
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return ListView(
+                    children: cities.map((city) {
+                      return ListTile(
+                        title: Text(city),
+                        onTap: () {
+                          Navigator.pop(context);
+                          _onCitySelected(city);
+                        },
+                      );
+                    }).toList(),
+                  );
+                },
+              );
+            },
+            icon: const Icon(Icons.filter_list, color: Colors.white),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(color: Colors.grey.shade400),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.blue.shade200),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+              ),
+              onSubmitted: _onSearchSubmitted,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              "Recently Searched",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: recentSearches.map((search) {
+                  return GestureDetector(
+                    onTap: () => _onRecentSearchTapped(search),
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 8.0),
+                      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [Colors.blue, Colors.purple],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Text(
+                        search,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            const Divider(color: Colors.grey),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: searchResults.length,
+                itemBuilder: (context, index) {
+                  final hospital = searchResults[index];
+                  return GestureDetector(
+                    onTap: () => _onCardTap(hospital),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      elevation: 3,
+                      margin: const EdgeInsets.only(bottom: 16),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset(
+                                hospital["image"]!,
+                                height: 150,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              hospital["name"]!,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              hospital["location"]!,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/admin_screen/main.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:tess/admin_screen/edit_profiles/edit_profiles_main.dart';
+
+
+void main() {
+  runApp(const AdminScreen());
+}
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  final double imageSize = 50.0; // Icon size for admin features
+  final double fontSize = 12.0; // Font size for labels
+
+  // Updated labels for admin functionality
+  final List<String> labels = [
+    "Edit Profiles",
+    "Add New Entry",
+    "Add Patient",
+    "Add Doctor",
+    "Reports",
+    "Settings",
+  ];
+
+  // Updated images for corresponding admin functionality
+  final List<String> images = [
+    "images/user_management.png",
+    "images/add_hospital.png",
+    "images/add_patient.png",
+    "images/add_doctor.png",
+    "images/reports.png",
+    "images/settings.png",
+  ];
+
+  final DateFormat dateFormat = DateFormat('MMM d, yyyy');
+
+  // Navigation handling updated for admin-specific screens
+  void handleOnTap(String label, BuildContext context) {
+    switch (label) {
+      case "Edit Profiles":
+        Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const edit_profiles_main()),
+                  );
+        break;
+      case "Add New Entry":
+
+        break;
+      case "Add Patient":
+  
+        break;
+      case "Add Doctor":
+
+        break;
+      case "Reports":
+
+        break;
+      case "Settings":
+        
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.symmetric(vertical: 12.0),
+          margin: const EdgeInsets.symmetric(horizontal: 32.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AdminScreen()),
+                  );
+                },
+                icon: Image.asset('images/icon1.png', width: 30, height: 30),
+              ),
+              IconButton(
+                onPressed: () {
+
+                },
+                icon: Image.asset('images/icon2.png', width: 30, height: 30),
+              ),
+              IconButton(
+                onPressed: () {
+
+                },
+                icon: Image.asset('images/icon3.png', width: 30, height: 30),
+              ),
+            ],
+          ),
+        ),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.asset(
+                          "images/Vector2.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(16.0, 60.0, 16.0, 20.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 75, // Profile image size
+                                  width: 75,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: AssetImage("images/father.jpg"),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 8.0),
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Hello Admin,",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Manage Your Platform",
+                                      style: TextStyle(
+                                        color: Color.fromARGB(207, 255, 255, 255),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Container(
+                                constraints: const BoxConstraints(maxWidth: 500),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(24.0),
+                                  border: Border.all(color: Colors.grey[300]!, width: 1),
+                                ),
+                                padding: const EdgeInsets.all(12.0),
+                                child: Column(
+                                  children: [
+                                    for (int row = 0; row < 2; row++) ...[
+                                      if (row > 0)
+                                        const Divider(color: Colors.grey, thickness: 1),
+                                      Row(
+                                        children: [
+                                          for (int col = 0; col < 3; col++) ...[
+                                            if (col > 0)
+                                              Container(
+                                                width: 1,
+                                                color: Colors.grey[300],
+                                                height: 80,
+                                              ),
+                                            Expanded(
+                                              child: Builder(
+                                                builder: (BuildContext innerContext) {
+                                                  return InkWell(
+                                                    onTap: () => handleOnTap(
+                                                        labels[(row * 3) + col], innerContext),
+                                                    child: Column(
+                                                      mainAxisAlignment: MainAxisAlignment.center,
+                                                      children: [
+                                                        Container(
+                                                          height: imageSize,
+                                                          width: imageSize,
+                                                          child: Image.asset(
+                                                            images[(row * 3) + col],
+                                                            fit: BoxFit.contain,
+                                                          ),
+                                                        ),
+                                                        const SizedBox(height: 6),
+                                                        Text(
+                                                          labels[(row * 3) + col],
+                                                          textAlign: TextAlign.center,
+                                                          style: TextStyle(
+                                                            fontSize: fontSize,
+                                                            color: Colors.black,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  );
+                                                },
+                                              ),
+                                            ),
+                                          ],
+                                        ],
+                                      ),
+                                    ],
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                      constraints: const BoxConstraints(maxWidth: 500),
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24.0),
+                        border: Border.all(color: Colors.grey[300]!, width: 1),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Latest Updates",
+                            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            "View recent activity, monitor claims, and manage users to ensure smooth operations.",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
